3b3d0427690d8c08af31bd50b924b4107d4968ba
bikeonastick <bikeonastick@gmail.com>
2017-05-14 22:40:47 -0500

    Working library with partial README and LICENSE

---
+++ README.md
@@ -0,0 +1,41 @@
+# GAS Query
+
+This library is a workaround for the fact that you cannot use Google's awesome
+query language directly in a Google Apps Script for your Google Sheets
+scripting. Use this library to execute a `=QUERY` formula in Google Sheets and
+return the results as a JavaScript object to iterate over. 
+
+What GAS Query does is it creates a sheet, puts the query you pass it into cell
+A1, iterates over the results, and puts it into a JavaScript object.
+
+## Limitations
+
+The logic for creating your JavaScript object expects that the first row of your
+QUERY will be a row of headings. It usese those values to create keys in your
+JavaScript object. 
+
+## Testing
+
+Currently, testing of Google App Scripts is not yet well defined. As I live with
+this and it grows, I will pick a good testing framework. Right now, as all the
+code fits on my 13" screen, I'm comfortable saying, "We've got this," however,
+it won't take much more code for me to change my answer.
+
+## How to use GAS Query in your Google Sheet
+
+Currently, there is no real standard way to share libraries in GAS. You will
+need to check out this repo, copy the code from gas_query.js into your project
+and reference the library, see below. 
+
+## Contributing
+
+1. Fork the repo
+1. Make changes
+1. Test changes
+1. Submit a pull request
+
+# References
+
+- https://developers.google.com/apps-script/guide_libraries
+- https://developers.google.com/apps-script/guide_libraries#using-a-library
+
---
+++ gas_query.js
@@ -0,0 +1,84 @@
+//function onOpen() { 
+//  var spreadsheet = SpreadsheetApp.getActive();
+//  var menuItems = [
+//    {name: 'TestLibrary', functionName: 'testLib_'}
+//  ];
+//  spreadsheet.addMenu('Test', menuItems);
+//}
+//
+//function testLib_(){
+//  // query to run
+//  var myQuery = "=QUERY(Sheet1!A1:O,\"SELECT B, F, C, L, M, N, K WHERE D = 'Q2-2017'\")";
+//  // object
+//  
+//  var filledObj = runQuery(myQuery);
+//  Logger.log("data");
+//   
+//  //Logger.log(filledObj.allData);
+//  Logger.log(filledObj.getHeadings());
+//  Logger.log("obj:" + filledObj.getRowObj(0).get('ready score'));
+//  Logger.log("number of rows=" + filledObj.numRows());
+//  Logger.log("number of cols=" + filledObj.numColumns());
+//}
+
+/**
+ * Workaround for the fact that you cannot execute the =QUERY macro from Javascript. Works
+ * by creating a temporary sheet, dumping your query into it, putting the results into a
+ * JavaScript object, and deleting the sheet. REQUIRES your data has headers at the top. If
+ * you are getting 'undefined' when calling get on a row, you may need to add a 1 to the end of
+ * the query you are passing in: -QUERY(Sheet1!A1:D,"SELECT A, C, D WHERE B = 'sad'",1) The last
+ * argument is there to tell the query engine that the top row of data is header, so it's 
+ * included in the returned dataset. 
+ * 
+ * @param {string} a functional =QUERY macro for data within your spreadsheet
+ * @return {object} an object with convenience methods for accessing the data. Call getRowObj on the 
+ * returned object and each row object has convenience methods to either get the row as an array (rowArr())
+ * or to just get values by name, e.g., retObj.getRowObj().get('column heading name'). 
+ */
+function runQuery(query) {
+  var ss = SpreadsheetApp.getActive();
+  var now = new Date();
+  var tempSheetName = "temp"+ now.getTime();
+  var tempSheet = ss.insertSheet(tempSheetName);
+  tempSheet.getRange(1, 1).setFormula(query);
+  var lastCol = tempSheet.getLastColumn();
+  var lastRow = tempSheet.getLastRow();
+  
+  var sheetData = tempSheet.getRange(1, 1, lastRow, lastCol);
+  
+  var retObj = {
+    allData: sheetData.getValues(),
+    getHeadings: function() { return retObj.allData[0]; },
+    getRowArr: function(rowIx) { 
+      //compensate for headings row
+      return retObj.allData[rowIx + 1]; 
+    },
+    getRowObj: function(rowIx) { 
+      var rowObj = {
+        rowArr: function() {return retObj.getRowArr(rowIx);},
+        headings: function() { return retObj.getHeadings();},
+        get: function(item) {
+          var headIx = rowObj.headings().indexOf(item);
+          return rowObj.rowArr()[headIx];
+        }
+      };
+      return rowObj; 
+    },
+    getHeadingIndex: function(heading){ 
+      var hr = retObj.getHeadings();
+      return hr.indexOf(heading);
+    },
+    numRows: function() {
+      return retObj.allData.length -1;
+    },
+    numColumns: function() {
+      return retObj.allData[0].length;
+    }
+  };
+  
+  ss.deleteSheet(tempSheet);
+  
+  return retObj;
+ 
+}
+
